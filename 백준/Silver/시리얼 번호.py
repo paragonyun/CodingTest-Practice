82c0ecc0f6b633577a16198909adad11f8b0f5be
Jeong SeongYun <83996346+paragonyun@users.noreply.github.com>
2023-03-03 19:33:31 +0900

    Update 시리얼 번호.py

--- 백준/Silver/시리얼 번호.py
+++ 백준/Silver/시리얼 번호.py
@@ -1,1 +1,16 @@
+n = int(input())
 
+series = [input() for _ in range(n)]
+
+def summing(x):
+    result = 0
+    for i in x:
+        if i.isdigit():
+            result += int(i)
+    
+    return result
+
+series = sorted(series, key=lambda x: (len(x), summing(x), x))
+
+for i in series:
+    print(i)
